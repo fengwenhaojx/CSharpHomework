87c05d1875dcb36471a518a511586bb4b296f93e
fengwenhaojx <2946658593@qq.com>
2018-09-17 22:52:41 +0800

    homework2

---
+++ CSharpHomework/homework2/program2/program1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//输出用户指定数据的所有素数因子
+namespace program1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please input an int number: ");
+            string s = Console.ReadLine();
+            int num;
+            bool vaild = Int32.TryParse(s, out num);
+            if (vaild)
+            {
+                Console.WriteLine("Its prime factors are: ");
+                for (int i = 2; i <= num; i++)
+                {
+                    if (IsPrimeOrNot(i) && num % i == 0)
+                        Console.Write(i + " ");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Input error!");
+            }
+        }
+        private static bool IsPrimeOrNot(int a)
+        {
+            for (int i = 2; i < a; i++)
+            {
+                if (a % i == 0) return false;
+            }
+            return true;
+        }
+    }
+}
---
+++ CSharpHomework/homework2/program2/program2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//求一个整数数组的最大值，最小值，平均值，和
+namespace program2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 3, 54, 4, 7, 34, 23, 87, 3 };
+            int max=arr[0], min=arr[0];
+            double avr=0, he = 0;
+            for(int i=0;i<arr.Length;i++)
+            {
+                if (arr[i] > max) max = arr[i];
+                if (arr[i] < min) min = arr[i];
+                he += arr[i];
+            }
+            avr = he / arr.Length;
+            Console.WriteLine("数组最大值是：" + max);
+            Console.WriteLine("数组最小值是：" + min);
+            Console.WriteLine("数组元素的和是：" + he);
+            Console.WriteLine("数组元素的平均值是：" + avr);
+        }
+        
+    }
+}
