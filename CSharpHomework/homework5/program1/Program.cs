a52e0ddcd176ff42c44a53921624f7b2a4119af3
fengwenhaojx <2946658593@qq.com>
2018-10-15 22:09:22 +0800

    homework5

---
+++ CSharpHomework/homework5/program1/Program.cs
@@ -0,0 +1,228 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+public class OrderDetails
+{
+    static int orders = 0;
+    public int orderNumber;
+    public string orderName;
+    public string orderOwner;
+    public int orderMoney;
+    public OrderDetails()
+    {
+        OrderDetails.orders++;
+        this.orderNumber = OrderDetails.orders;
+    }
+    public OrderDetails(string orName, string orOwner,int orMoney)
+    {
+        OrderDetails.orders++;
+        this.orderNumber = OrderDetails.orders;
+        this.orderName = orName;
+        this.orderOwner = orOwner;
+        this.orderMoney = orMoney;
+    }
+    public void print()
+    {
+        Console.WriteLine(this.orderNumber);
+        Console.WriteLine(this.orderName);
+        Console.WriteLine(this.orderOwner);
+        Console.WriteLine(this.orderMoney);
+    }
+    public void change(string orName, string orOwner,int orMoney)
+    {
+        this.orderName = orName;
+        this.orderOwner = orOwner;
+        this.orderMoney = orMoney;
+    }
+}
+public class Order
+{
+    public List<OrderDetails> orderList = new List<OrderDetails>();
+    public void PrintOrder()
+    {
+        Console.WriteLine("******************************************");
+        for (int i = 0; i < orderList.Count; i++)
+        {
+            orderList[i].print();
+        }
+        Console.WriteLine("******************************************");
+    }
+}
+public class OrderService
+{
+    public void addOneOrder(ref Order order)
+    {
+        OrderDetails e = new OrderDetails();
+        Console.WriteLine("请输入商品名称：");
+        e.orderName = Console.ReadLine();
+        Console.WriteLine("请输入客户名称：");
+        e.orderOwner = Console.ReadLine();
+        Console.WriteLine("请输入商品金额：");
+        e.orderMoney = Int32.Parse(Console.ReadLine());
+        order.orderList.Add(e);
+    }
+    public void findOrderByNumber(Order order)
+    {
+        Console.WriteLine("请输入订单号：");
+        int num = Int32.Parse(Console.ReadLine());
+        for (int i = 0; i < order.orderList.Count; i++)
+        {
+            if (order.orderList[i].orderNumber == num)
+            {
+                Console.WriteLine("******************************************");
+                order.orderList[i].print();
+                Console.WriteLine("******************************************");
+                return;
+            }
+        }
+        Console.WriteLine("没有此订单号的商品！");
+    }
+    public void findOrderByName(Order order)
+    {
+        Console.WriteLine("请输入需查询商品的名称：");
+        string orName = Console.ReadLine();
+        var m = from n in order.orderList
+                where n.orderName.Equals(orName)
+                select n;
+        int x = 0;
+        foreach (var i in m)
+        {
+            x++;
+        }
+        if (x == 0)
+        {
+            Console.WriteLine("没有此名称的商品！");
+            return;
+        }
+        Console.WriteLine("******************************************");
+        foreach (var i in m)
+        {
+            i.print();
+        }
+        Console.WriteLine("******************************************");
+    }
+    public void findOrderByOwner(Order order)
+    {
+        Console.WriteLine("请输入客户的名称：");
+        string orOwner = Console.ReadLine();
+        var m = from n in order.orderList
+                where n.orderOwner.Equals(orOwner)
+                select n;
+        int x = 0;
+        foreach (var i in m)
+        {
+            x++;
+        }
+        if (x == 0)
+        {
+            Console.WriteLine("没有此客户的商品！");
+            return;
+        }
+        Console.WriteLine("******************************************");
+        foreach (var i in m)
+        {
+            i.print();
+        }
+        Console.WriteLine("******************************************");
+    }
+    public void findOrderOver10000(Order order)
+    {
+        var m = from n in order.orderList
+                where n.orderMoney>=10000
+                select n;
+        int x = 0;
+        foreach (var i in m)
+        {
+            x++;
+        }
+        if (x == 0)
+        {
+            Console.WriteLine("没有超过10000的商品！");
+            return;
+        }
+        Console.WriteLine("******************************************");
+        foreach (var i in m)
+        {
+            i.print();
+        }
+        Console.WriteLine("******************************************");
+    }
+    public void ChangeByNumber(ref Order order)
+    {
+        Console.WriteLine("请输入订单号：");
+        int num = Int32.Parse(Console.ReadLine());
+        int i;
+        for (i = 0; i < order.orderList.Count; i++)
+        {
+            if (order.orderList[i].orderNumber == num)
+            {
+                Console.WriteLine("请重新输入商品名称：");
+                order.orderList[i].orderName = Console.ReadLine();
+                Console.WriteLine("请重新输入客户名称：");
+                order.orderList[i].orderOwner = Console.ReadLine();
+                Console.WriteLine("请重新输入商品金额：");
+                order.orderList[i].orderMoney = Int32.Parse(Console.ReadLine());
+                return;
+            }
+        }
+        Console.WriteLine("没有此订单号的商品！");
+    }
+    public void deleteByNumber(ref Order order)
+    {
+        Console.WriteLine("请输入订单号：");
+        int num = Int32.Parse(Console.ReadLine());
+        for (int i = 0; i < order.orderList.Count; i++)
+        {
+            if (order.orderList[i].orderNumber == num)
+            {
+                order.orderList.RemoveAt(i);
+                return;
+            }
+        }
+        Console.WriteLine("没有此订单号的商品！");
+    }
+}
+namespace program2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int od = 1;
+            Order order1 = new Order();
+            OrderService os = new OrderService();
+            while (od != 0)
+            {
+                switch (od)
+                {
+                    case 1:
+                        os.addOneOrder(ref order1);
+                        break;
+                    case 2:
+                        os.ChangeByNumber(ref order1);
+                        break;
+                    case 3:
+                        os.deleteByNumber(ref order1);
+                        break;
+                    case 4:
+                        os.findOrderByNumber(order1);
+                        break;
+                    case 5:
+                        order1.PrintOrder();
+                        break;
+                    case 6:
+                        os.findOrderByName(order1);
+                        break;
+                    case 7:
+                        os.findOrderByOwner(order1);
+                        break;
+                    case 8:
+                        os.findOrderOver10000(order1);
+                        break;
+                }
+                Console.WriteLine("1（添加商品）；2（修改商品）；3（删除商品）；4（按订单号查询）；5（打印订单）；6（按名称查询）；7（按客户查询）；8（查找金额大于一万的订单）");
+                od = Int32.Parse(Console.ReadLine());
+            }
+        }
+    }
+}
---
+++ CSharpHomework/homework5/program2/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace program2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private Graphics graphics;
+        double th1;
+        double th2;
+        double per1;
+        double per2;
+        double positionPer1;
+        double positionPer2;
+        Pen col;
+        private void draw_button_Click(object sender, EventArgs e)
+        {
+            this.th1 = Double.Parse(textBox1.Text) * Math.PI / 180;
+            this.th2 = Double.Parse(textBox2.Text) * Math.PI / 180;
+            this.per1 = Double.Parse(textBox3.Text);
+            this.per2 = Double.Parse(textBox4.Text);
+            this.positionPer1 = Double.Parse(textBox5.Text);
+            this.positionPer2 = Double.Parse(textBox6.Text);
+            if (graphics == null) graphics = this.CreateGraphics();
+            drawCayleyTree(10, 200, 310, 100, -Math.PI / 2);
+        }
+        private void drawCayleyTree(int n,double x0,double y0,double length,double th)
+        {
+            if (n == 0) return;
+            double x1 = x0 + length * Math.Cos(th);
+            double y1 = y0 + length * Math.Sin(th);
+            drawLine(x0, y0, x1, y1);
+            double x2 = x0 + length *positionPer1* Math.Cos(th);
+            double y2 = y0 + length *positionPer1* Math.Sin(th);
+            double x3 = x0 + length *positionPer2* Math.Cos(th);
+            double y3 = y0 + length *positionPer2* Math.Sin(th);
+            drawCayleyTree(n - 1, x2, y2, length * per1, th + th1);
+            drawCayleyTree(n - 1, x3, y3, length * per2, th - th2);
+        }
+        private void drawLine(double x0,double y0,double x1,double y1)
+        {
+            graphics.DrawLine(col, (int)x0, (int)y0, (int)x1, (int)y1);
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch(listBox1.Text)
+            {
+                case "Red":
+                    col = Pens.Red;
+                    break;
+                case "Yellow":
+                    col = Pens.Yellow;
+                    break;
+                case "Blue":
+                    col = Pens.Blue;
+                    break;
+            }
+        }
+    }
+}
