4cb509ddc2741f34f630cf74a920e5ee52a90202
fengwenhaojx <2946658593@qq.com>
2018-11-15 15:47:20 +0800

    homework8

---
+++ CSharpHomework/homework8/program1/ksfdjkas.xsl
@@ -0,0 +1,33 @@
+﻿<?xml version="1.0" encoding="UTF-8"?>
+
+<xsl:stylesheet version="1.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform">
+<xsl:template match="/">
+<html>
+<body>
+<h4>订单</h4>
+<table>
+    <tr>
+      <th align ="left">订单号</th>
+      <th align ="left">流水号</th>
+      <th align ="left">商品名称</th>
+      <th align ="left">客户名称</th>
+      <th align ="left">商品金额</th>
+      <th align ="left">客户电话</th>
+      <th align ="left">商品日期</th>
+    </tr>
+    <xsl:for-each select="Order/orderList/OrderDetails">
+    	<tr>
+	<td><xsl:value-of select="orderNumber"/></td>
+	<td><xsl:value-of select="orderBehindNum"/></td>
+	<td><xsl:value-of select="orderName"/></td>
+	<td><xsl:value-of select="orderOwner"/></td>
+	<td><xsl:value-of select="orderMoney"/></td>
+	<td><xsl:value-of select="phoneNumber"/></td>
+	<td><xsl:value-of select="orderDate"/></td>
+	</tr>
+    </xsl:for-each>
+</table>
+</body>
+</html>
+</xsl:template>
+</xsl:stylesheet>
---
+++ CSharpHomework/homework8/program2/Form1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using program1;
+using Microsoft.VisualBasic;
+namespace program2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+            
+        }
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                
+                form2.ShowDialog();
+                OrderDetails newOrder = new OrderDetails(Int32.Parse(form2.textBox1.Text)
+                    , form2.textBox2.Text
+                    , form2.textBox3.Text
+                    , Int32.Parse(form2.textBox4.Text)
+                    ,form2.textBox5.Text);
+                service.addOneOrder(ref order, newOrder);
+                listBox1.Items.Add(newOrder.orderName);
+            }
+            catch(Exception ex)
+            {
+                textBox6.Text = ex.Message;
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedItem != null)
+            {
+                OrderDetails dd = order.orderList[listBox1.SelectedIndex];
+                string s = dd.orderNumber
+                    +"\r\n"+dd.orderName
+                    +"\r\n"+dd.orderOwner
+                    +"\r\n"+dd.orderMoney
+                    +"\r\n" + dd.phoneNumber;
+
+                textBox5.Text = s;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                form2.ShowDialog();
+                for (int i = 0; i < order.orderList.Count; i++)
+                {
+                    if (order.orderList[i].orderNumber == long.Parse(form2.textBox1.Text))
+                    {
+                        listBox1.Items.RemoveAt(i);
+                    }
+                }
+                service.deleteByNumber(ref order, long.Parse(form2.textBox1.Text));
+            }
+            catch(Exception ex)
+            {
+                textBox6.Text = ex.Message;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                form2.ShowDialog();
+                OrderDetails dd = service.findOrderByNumber(order, Int32.Parse(form2.textBox1.Text));
+                string s = dd.orderNumber
+                    + "\r\n" + dd.orderName
+                    + "\r\n" + dd.orderOwner
+                    + "\r\n" + dd.orderMoney
+                    + "\r\n" + dd.phoneNumber;
+                for(int i=0;i<order.orderList.Count;i++)
+                {
+                    if(order.orderList[i].orderNumber== Int32.Parse(form2.textBox1.Text))
+                    {
+                        listBox1.SelectedIndex = i;
+                        break;
+                    }
+                }
+                textBox5.Text = s;
+            }
+            catch (Exception ex)
+            {
+                textBox6.Text = ex.Message;
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                form2.ShowDialog();
+                OrderDetails newOrder = new OrderDetails(Int32.Parse(form2.textBox1.Text)
+                    , form2.textBox2.Text
+                    , form2.textBox3.Text
+                    , Int32.Parse(form2.textBox4.Text)
+                    ,form2.textBox5.Text);
+                service.ChangeByNumber(ref order,order.orderList[listBox1.SelectedIndex].orderNumber, newOrder);
+                listBox1.Items[listBox1.SelectedIndex] = form2.textBox2.Text;
+            }
+            catch (Exception ex)
+            {
+                textBox6.Text = ex.Message;
+            }
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
